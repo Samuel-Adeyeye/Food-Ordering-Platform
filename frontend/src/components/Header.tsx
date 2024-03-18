00f5b6b0a11f3e3c1fd55c593f6a8d9460f913d3
Samuel <36992355+Samuel-Adeyeye@users.noreply.github.com>
2024-03-18 18:23:02 +0100

    feat: :sparkles: set up homepage
    Setup homepage components and layouts

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
   return (
@@ -6,7 +8,13 @@ const Header = () => {
           <div className="container mx-auto flex justify-between items-center">
               <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
                   HomeMeals.com
-              </Link>
+        </Link>
+        <div className="md:hidden">
+          <MobileNav/>
+        </div>
+        <div className="hidden md:block">
+          <MainNav/>
+        </div>
           </div>
     </div>
   )
