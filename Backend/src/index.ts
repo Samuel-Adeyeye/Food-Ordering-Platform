ccc21317a9e5de2a0ebdd5007290f79ebb21ecd5
Samuel <36992355+Samuel-Adeyeye@users.noreply.github.com>
2024-04-03 10:56:16 +0100

    create user request api

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,22 +2,24 @@ import express, {Request, Response} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import myUserRoute from './routes/MyUserRoute';
 
 dotenv.config()
 
 const connectMongoDB = async () => {
-    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => console.log("Connected to database!"));
-};
-
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+        .then(() => console.log("Connected to database!"));
+        
+}
+console.log(connectMongoDB)
 connectMongoDB();
+
     
 const app = express();
 app.use(express.json());
 app.use(cors())
 
-app.get('/test', async (req:Request, res:Response) => {
-    res.json({message: "Hello!"})
-});
+app.use("/api/my/user", myUserRoute)
 
 app.listen(7000, () => {
     console.log("Server started on localhost: 7000")
