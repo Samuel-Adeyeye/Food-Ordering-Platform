ec8f2f9cf1f6bf0bd3fd3e5d52b0445f5e7431bc
Samuel <36992355+Samuel-Adeyeye@users.noreply.github.com>
2024-05-06 20:01:14 +0100

    update User Profile Request

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,10 +1,11 @@
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 const UserProfilePage = () => { 
+    const {updateUser, isLoading } = useUpdateMyUser();
     return (
-        <UserProfileForm onSave={function (): void {
-            throw new Error("Function not implemented.");
-        } } isLoading={false}/>
+        <UserProfileForm onSave={updateUser} 
+         isLoading={isLoading}/>
     )
 }; 
 
